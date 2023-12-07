4c948163a921428ef47e5ceed896db00845b0478
gonzalogacc <gonzalogacc@gmail.com>
2023-12-07 12:21:59 +0100

    some stuff

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ import os
 import logging
 
 my_config = Config(region_name = 'eu-west-1')
-try:
-    print("Got creds from ECS")
-    resource = boto3.resource('dynamodb', config=my_config)
-except:
-    print("Failed to get creds from ECS, reading from env")
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', None)
-    print(f"ID populated: {AWS_ACCESS_KEY_ID is not None}")
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
-    print(f"Key populated: {AWS_SECRET_ACCESS_KEY is not None}")
-    client = boto3.client('dynamodb', 
-                          config=my_config,
-                          aws_access_key_id=AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+#try:
+#    print("Got creds from ECS")
+#    resource = boto3.resource('dynamodb', config=my_config)
+#except:
+#    print("Failed to get creds from ECS, reading from env")
+#    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', None)
+#    print(f"ID populated: {AWS_ACCESS_KEY_ID is not None}")
+#    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
+#    print(f"Key populated: {AWS_SECRET_ACCESS_KEY is not None}")
+#    client = boto3.client('dynamodb', 
+#                          config=my_config,
+#                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+#                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 app = FastAPI()
 
